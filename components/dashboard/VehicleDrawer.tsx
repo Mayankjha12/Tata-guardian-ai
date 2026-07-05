5a83c6eb8b88b70b7d2e9851bdda8ec9a08b8d9f
Mayankjha12 <mayank.jha.ug24@nsut.ac.in>
2026-07-05 15:58:09 +0530

    Update VehicleDrawer.tsx

--- components/dashboard/VehicleDrawer.tsx
+++ components/dashboard/VehicleDrawer.tsx
@@ -66,7 +66,9 @@ export function VehicleDrawer({ vehicle }: VehicleDrawerProps) {
         </div>
         <div className="glass rounded-lg p-3 space-y-1">
           <p className="text-xs text-neutral-500 uppercase">Location</p>
-          <p className="text-xs">{vehicle.location.address.split(',')[0]}</p>
+          <p className="text-xs">
+  {vehicle.location.address?.split(",")[0] ?? "Unknown Location"}
+</p>
         </div>
       </div>
 
